0eb9e876caa2a592a4e1e319b3e31b9346c28faf
Joey Seeman <joeymsee@gmail.com>
2026-04-04 23:21:49 -0700

    Direction test complete.

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -3,6 +3,8 @@ package nl.tudelft.jpacman.board;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * A very simple (and not particularly useful)
@@ -14,9 +16,11 @@ public class DirectionTest {
     /**
      * Do we get the correct delta when moving north?
      */
-    @Test
-    void testNorth() {
-        Direction north = Direction.valueOf("NORTH");
-        assertThat(north.getDeltaY()).isEqualTo(-1);
+    @ParameterizedTest
+    @CsvSource({"NORTH,0,-1", "EAST,1,0", "SOUTH,0,1", "WEST,-1,0"})
+    void testDirection(String directionString, int expectedX, int expectedY) {
+        Direction direction = Direction.valueOf(directionString);
+        assertThat(direction.getDeltaX()).isEqualTo(expectedX);
+        assertThat(direction.getDeltaY()).isEqualTo(expectedY);
     }
 }
